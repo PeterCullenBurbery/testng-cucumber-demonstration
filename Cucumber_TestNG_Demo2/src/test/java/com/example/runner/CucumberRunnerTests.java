593f928874542a784dc440c491ae73e048de5dcf
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2026-01-05 15:30:26 -0500

    Add Cucumber TestNG runner for LoginPage feature
    Introduces CucumberRunnerTests to execute LoginPage.feature using TestNG. Sets up CucumberOptions with feature path and glue code location.

---
+++ Cucumber_TestNG_Demo2/src/test/java/com/example/runner/CucumberRunnerTests.java
@@ -0,0 +1,9 @@
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(tags = "", features = {"src/test/resources/features/LoginPage.feature"}, glue = {"com.example.definitions"},
+        plugin = {})
+
+public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
+
+}
