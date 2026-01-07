abe198f621a006c020d3c84c8403dd9fbf70baa6
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2026-01-07 17:54:46 -0500

    Add new Maven project for Amazon Cucumber TestNG demo
    Created the Cucumber_TestNG_Demo_Amazon_2 Maven project with initial structure, configuration files, and a basic App and AppTest class. Also updated Eclipse settings and classpath in the existing Cucumber_TestNG_Demo project to support annotation processing.

---
+++ Cucumber_TestNG_Demo_Amazon_2/src/main/java/com/example/Cucumber_TestNG_Demo_Amazon_2/App.java
@@ -0,0 +1,10 @@
+package com.example.Cucumber_TestNG_Demo_Amazon_2;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
