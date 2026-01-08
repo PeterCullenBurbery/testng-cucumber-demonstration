72f914cd9effdc4441448d029295cb73da83ff9e
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2026-01-08 14:49:23 -0500

    Initialize Maven Java project with Selenium and JUnit
    Set up a new Maven-based Java project for Eclipse with Selenium and JUnit dependencies. Includes basic project structure, configuration files, and a sample main class and test.

---
+++ Cucumber_TestNG_Demo_Amazon_Screenshots_3_Slash_Underscore_1/src/test/java/com/example/Cucumber_TestNG_Demo_Amazon_Screenshots_3_Slash_Underscore_1/AppTest.java
@@ -0,0 +1,19 @@
+package com.example.Cucumber_TestNG_Demo_Amazon_Screenshots_3_Slash_Underscore_1;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
