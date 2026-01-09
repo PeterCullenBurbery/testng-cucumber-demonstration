aade2a700d1fee69892704ae0cb10c5fc464133c
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2026-01-09 16:23:55 -0500

    Initialize Maven Java project with basic structure
    Add initial project files for Extent_Reports_5_Cucumber_7_TestNG, including Maven configuration, Eclipse settings, a simple App class, and a basic JUnit test. This sets up the foundation for further development with Selenium and JUnit dependencies.

---
+++ Extent_Reports_5_Cucumber_7_TestNG/src/main/java/com/example/Extent_Reports_5_Cucumber_7_TestNG/App.java
@@ -0,0 +1,10 @@
+package com.example.Extent_Reports_5_Cucumber_7_TestNG;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
