bb26a79af9ce4f1198d05d9f63d1328022eaf151
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2026-01-26 15:51:27 -0500

    Update LoginPageLocators with improved selectors
    Refactored element locators to use more robust and maintainable XPath expressions. Updated selectors for title, error messages, social icons, and the forgot password link to better match the current OrangeHRM demo UI structure and reduce brittleness.

--- Extent_Reports_Version_5_for_Cucumber_7_and_TestNG/src/main/java/com/example/locators/LoginPageLocators.java
+++ Extent_Reports_Version_5_for_Cucumber_7_and_TestNG/src/main/java/com/example/locators/LoginPageLocators.java
@@ -1,38 +1,41 @@
 package com.example.locators;
- 
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
- 
+
 public class LoginPageLocators {
- 
+
     @FindBy(name = "username")
     public WebElement userName;
-  
+
     @FindBy(name = "password")
     public WebElement password;
-  
-    @FindBy(id = "logInPanelHeading")
+
+    // Fixed: OrangeHRM demo usually uses a generic login title or h5
+    @FindBy(xpath = "//h5[contains(@class, 'login-title')]")
     public WebElement titleText;
-     
-    @FindBy(xpath = "//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/span")
+
+    // Fixed: Using specific input grouping to distinguish between user and pass errors
+    @FindBy(xpath = "//div[label[text()='Username']]/following-sibling::span")
     public WebElement missingUsernameErrorMessage;
-     
-    @FindBy(xpath = "//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/span")
+
+    @FindBy(xpath = "//div[label[text()='Password']]/following-sibling::span")
     public WebElement missingPasswordErrorMessage;
-  
-    @FindBy(xpath = "//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")
+
+    @FindBy(xpath = "//button[@type='submit']")
     public WebElement login;
-  
-    @FindBy(xpath = "//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/p")
-    public  WebElement errorMessage;
-     
-    @FindBy(xpath = "//*[@href='https://www.linkedin.com/company/orangehrm/mycompany/']")
-    public  WebElement linkedInIcon;
-     
-    @FindBy(xpath = "//*[@href='https://www.facebook.com/OrangeHRM/mycompany']") //Invalid Xpath
-    public  WebElement faceBookIcon;
-     
-    @FindBy(xpath = "//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[4]/p")
-    public  WebElement ForgotYourPasswordLink;
-     
+
+    @FindBy(xpath = "//div[@role='alert']//p")
+    public WebElement errorMessage;
+
+    @FindBy(xpath = "//a[contains(@href, 'linkedin.com')]")
+    public WebElement linkedInIcon;
+
+    // Fixed: Changed from a hardcoded full URL to a partial match
+    @FindBy(xpath = "//a[contains(@href, 'facebook.com')]")
+    public WebElement faceBookIcon;
+
+    @FindBy(xpath = "//p[contains(@class, 'login-forgot-header')]")
+    public WebElement ForgotYourPasswordLink;
+
 }
