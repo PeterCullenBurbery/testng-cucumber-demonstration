d4d1a06282c86e9ec1909f4daeaf7607c290a1d3
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2026-01-26 15:24:09 -0500

    Initial project setup with Maven and sample code
    Add initial Maven project structure for Extent Reports integration with Cucumber 7 and TestNG. Includes basic Java source and test files, Eclipse configuration, and a sample App with a unit test.

---
+++ Extent_Reports_Version_5_for_Cucumber_7_and_TestNG/src/main/java/com/example/Extent_Reports_Version_5_for_Cucumber_7_and_TestNG/App.java
@@ -0,0 +1,10 @@
+package com.example.Extent_Reports_Version_5_for_Cucumber_7_and_TestNG;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Extent_Reports_Version_5_for_Cucumber_7_and_TestNG/src/test/java/com/example/Extent_Reports_Version_5_for_Cucumber_7_and_TestNG/AppTest.java
@@ -0,0 +1,19 @@
+package com.example.Extent_Reports_Version_5_for_Cucumber_7_and_TestNG;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
