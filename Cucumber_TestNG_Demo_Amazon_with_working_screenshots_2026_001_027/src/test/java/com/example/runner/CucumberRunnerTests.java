d30dc97d057f38a281929ccb2e4ef86f9bdd993e
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2026-01-27 16:07:42 -0500

    Add Amazon cart Cucumber step definitions and runner
    Introduces AmazonPageDefinitions with step implementations for searching, selecting, and adding products to the cart, including screenshot capture after each step. Adds CucumberRunnerTests to execute the AmazonCart.feature using TestNG.

---
+++ Cucumber_TestNG_Demo_Amazon_with_working_screenshots_2026_001_027/src/test/java/com/example/runner/CucumberRunnerTests.java
@@ -0,0 +1,12 @@
+package com.example.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+    features = {"src/test/resources/features/AmazonCart.feature"},
+    glue = {"com.example.definitions"},
+    plugin = {"pretty", "html:target/cucumber-reports.html"}
+)
+public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
+}
