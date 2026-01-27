c1128955896a4b88344d148af7aebdcfbcbde6c0
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2026-01-27 16:35:53 -0500

    Add ExtentReports plugin to Cucumber runner
    Included the ExtentCucumberAdapter plugin in the CucumberOptions to enable ExtentReports integration for enhanced test reporting.

--- Cucumber_TestNG_Demo_Amazon_with_working_screenshots_2026_001_027/src/test/java/com/example/runner/CucumberRunnerTests.java
+++ Cucumber_TestNG_Demo_Amazon_with_working_screenshots_2026_001_027/src/test/java/com/example/runner/CucumberRunnerTests.java
@@ -6,7 +6,11 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
     features = {"src/test/resources/features/AmazonCart.feature"},
     glue = {"com.example.definitions"},
-    plugin = {"pretty", "html:target/cucumber-reports.html"}
+    plugin = {
+        "pretty", 
+        "html:target/cucumber-reports.html",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" 
+    }
 )
 public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
 }
