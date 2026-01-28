bd9457b31d179bcb1e9d721ebb5c4e426b856a78
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2026-01-28 16:24:46 -0500

    Capture screenshots only on scenario failure
    Updated the @AfterStep hook to take and attach screenshots only when a scenario fails, reducing unnecessary screenshot captures and improving report relevance.

--- Cucumber_TestNG_Demo_Amazon_with_working_screenshots_that_takes_screenshot_only_on_failure_2026_001_028/src/test/java/com/example/definitions/AmazonPageDefinitions.java
+++ Cucumber_TestNG_Demo_Amazon_with_working_screenshots_that_takes_screenshot_only_on_failure_2026_001_028/src/test/java/com/example/definitions/AmazonPageDefinitions.java
@@ -13,8 +13,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Scenario;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 
 import java.time.Duration;
 
@@ -108,13 +106,15 @@ public class AmazonPageDefinitions {
         // if (driver != null) driver.quit();
     }
     
-    @AfterStep
+@AfterStep
     public void add_screenshot(Scenario scenario) {
-        // validate if driver is initialized before taking screenshot
-        if (driver != null) {
-            final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-            // Attach the screenshot to the Cucumber report
-            scenario.attach(screenshot, "image/png", "image_at_step_" + scenario.getLine());
+        // Only take a screenshot if the scenario has failed
+        if (scenario.isFailed()) {
+            if (driver != null) {
+                final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+                // Attach the screenshot to the Cucumber report
+                scenario.attach(screenshot, "image/png", "failure_at_step_" + scenario.getLine());
+            }
         }
     }
 }
