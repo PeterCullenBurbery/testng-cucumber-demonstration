25810e16f413cff20ee0e7287dac1b3b302f5ba1
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2026-02-06 14:19:06 -0500

    Add Maven project scaffold for demo app
    Initial scaffold for Project_001_Cucumber_TestNG_Demo_focused_on_knife_and_other_stuff_2026_002_006: adds pom.xml (JUnit BOM, maven pluginManagement, Java release=25), basic App.java and AppTest.java, Eclipse project files (.project, .classpath, .settings), .mvn configs, and a .gitignore that excludes /target. Provides a minimal runnable example and IDE metadata for development.

---
+++ Project_001_Cucumber_TestNG_Demo_focused_on_knife_and_other_stuff_2026_002_006/src/main/java/com/example/Project_001_Cucumber_TestNG_Demo_focused_on_knife_and_other_stuff_2026_002_006/App.java
@@ -0,0 +1,10 @@
+package com.example.Project_001_Cucumber_TestNG_Demo_focused_on_knife_and_other_stuff_2026_002_006;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Project_001_Cucumber_TestNG_Demo_focused_on_knife_and_other_stuff_2026_002_006/src/test/java/com/example/Project_001_Cucumber_TestNG_Demo_focused_on_knife_and_other_stuff_2026_002_006/AppTest.java
@@ -0,0 +1,19 @@
+package com.example.Project_001_Cucumber_TestNG_Demo_focused_on_knife_and_other_stuff_2026_002_006;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
